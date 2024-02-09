760ab0d67a2416b5aa9a082893b1b0d5a1f4ee71
Rahul srivastava <87309230+Rahulsrivastava0124@users.noreply.github.com>
2024-02-09 12:20:43 +0530

    Update package versions, add bootstrap and typed.js, update html and App.js files. And add hero component

---
+++ src/Components/Home/Hero.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import Typed from 'typed.js'
+import Lesson from '../../Images/Lesson.gif'
+import C__ from "../../Images/c-.png";
+import C from "../../Images/c.png";
+import css3 from "../../Images/css-3.png";
+import html5 from "../../Images/html-5.png";
+import js from "../../Images/js.png";
+import python from "../../Images/python.png";
+import nodejs from "../../Images/node-js.png";
+import '../../CSS/Hero/Hero.css'
+
+
+
+export default function Hero() {
+    const el = React.useRef(null);
+    const LanguageList = [
+        '<span class="h2 text-primary" >React.js </span>',
+        '<span class="h2 text-success" >Node.js </span>',
+        '<span class="h2 text-danger" >C </span>',
+        '<span class="h2 text-warning"> JavaScript </span>',
+        '<span class="h2 text-primary" >HTML 5 </span>',
+        '<span class="h2 text-primary" >CSS 3 </span>'
+    ]
+    React.useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: LanguageList,
+            typeSpeed: 50,
+            loop: true,
+            cursorChar: "<span class='cursor h2 text-danger'>|</span>"
+        });
+        return () => {
+            // Destroy Typed instance during cleanup to stop animation
+            typed.destroy();
+        };
+        // eslint-disable-next-line
+    }, []);
+    return (
+        <section className='m-3 rounded-3 shadow'>
+            <div className="container  px-4 py-5 d-flex align-items-center justify-content-between">
+                <div className='container'>
+                    <h1>Learn Coding in <span className='text-primary'>Web</span ><span className='text-warning'>Dev</span></h1>
+                    <h2>
+                        Hello, What Do You Want To Learn?
+                    </h2>
+                    <span ref={el}></span>
+                    <div className='mt-5'>
+                        <button className='btn btn-primary  btn-lg me-3'>Learn Document </button>
+                        <button className='btn btn-warning btn-lg '>Course</button>
+                    </div>
+                </div>
+                <div className='container position-relative'>
+                    <img src={Lesson} className='' alt="Hero images" />
+                    <img src={C} className='float_image hero_image_1 position-absolute' alt="" />
+                    <img src={C__} className='float_image hero_image_2 position-absolute' alt="" />
+                    <img src={nodejs} className='float_image hero_image_3 position-absolute' alt="" />
+                    <img src={python} className='float_image hero_image_4 position-absolute' alt="" />
+                    <img src={js} className='float_image hero_image_5 position-absolute' alt="" />
+                    <img src={html5} className='float_image hero_image_6 position-absolute' alt="" />
+                    <img src={css3} className='float_image hero_image_7 position-absolute' alt="" />
+                </div>
+            </div>
+        </section>
+    )
+}
---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Hero from './Home/Hero'
+import { getPosition } from '../Function/Hero'
+export default function HomePage() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  )
+}
